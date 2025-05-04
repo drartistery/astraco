3a6f0e1f9113a890ecd0ab7addcafdea72a7e30c
drartistery <dr.artistery.official@gmail.com>
2025-05-04 15:33:42 +0530

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/@fs/home/runner/workspace/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=953ba777"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/@fs/home/runner/workspace/node_modules/.vite/deps/react-dom_client.js?v=953ba777"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx?v=mTnPHGfHarZyAWhmJ2vaD";
+import "/src/index.css?v=mTnPHGfHarZyAWhmJ2vaD";
+import { ThemeProvider } from "/src/context/ThemeContext.tsx?v=mTnPHGfHarZyAWhmJ2vaD";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(ThemeProvider, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "/home/runner/workspace/client/src/main.tsx?v=mTnPHGfHarZyAWhmJ2vaD",
+    lineNumber: 8,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "/home/runner/workspace/client/src/main.tsx?v=mTnPHGfHarZyAWhmJ2vaD",
+    lineNumber: 7,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBT0k7QUFQSixTQUFTQSxrQkFBa0I7QUFDM0IsT0FBT0MsU0FBUztBQUNoQixPQUFPO0FBQ1AsU0FBU0MscUJBQXFCO0FBRTlCRixXQUFXRyxTQUFTQyxlQUFlLE1BQU0sQ0FBRSxFQUFFQztBQUFBQSxFQUMzQyx1QkFBQyxpQkFDQyxpQ0FBQyxTQUFEO0FBQUE7QUFBQTtBQUFBO0FBQUEsU0FBSSxLQUROO0FBQUE7QUFBQTtBQUFBO0FBQUEsU0FFQTtBQUNGIiwibmFtZXMiOlsiY3JlYXRlUm9vdCIsIkFwcCIsIlRoZW1lUHJvdmlkZXIiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tIFwicmVhY3QtZG9tL2NsaWVudFwiO1xuaW1wb3J0IEFwcCBmcm9tIFwiLi9BcHBcIjtcbmltcG9ydCBcIi4vaW5kZXguY3NzXCI7XG5pbXBvcnQgeyBUaGVtZVByb3ZpZGVyIH0gZnJvbSBcIi4vY29udGV4dC9UaGVtZUNvbnRleHRcIjtcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcInJvb3RcIikhKS5yZW5kZXIoXG4gIDxUaGVtZVByb3ZpZGVyPlxuICAgIDxBcHAgLz5cbiAgPC9UaGVtZVByb3ZpZGVyPlxuKTtcbiJdLCJmaWxlIjoiL2hvbWUvcnVubmVyL3dvcmtzcGFjZS9jbGllbnQvc3JjL21haW4udHN4In0=
